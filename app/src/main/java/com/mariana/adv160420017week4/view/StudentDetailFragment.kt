1ee1f465cb60b710b15b9d614f48a4602ce36e4a
marianajunita17 <ymawuntu14@gmail.com>
2023-03-26 23:21:22 +0700

    tugas

--- app/src/main/java/com/mariana/adv160420017week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/mariana/adv160420017week4/view/StudentDetailFragment.kt
@@ -5,9 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mariana.adv160420017week4.R
+import com.mariana.adv160420017week4.viewmodel.DetailViewModel
+import com.mariana.adv160420017week4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel:DetailViewModel
+
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            txtID.setText(it.id)
+            txtName.setText(it.name)
+            txtBod.setText(it.dob)
+            txtPhone.setText(it.phone)
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +32,14 @@ class StudentDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
     }
 }

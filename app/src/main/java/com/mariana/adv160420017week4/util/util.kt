2836062ccb2b7468e1bd7f47276bcfb679be985d
marianajunita17 <ymawuntu14@gmail.com>
2023-04-12 15:09:21 +0700

    materi week 7

--- app/src/main/java/com/mariana/adv160420017week4/util/util.kt
+++ app/src/main/java/com/mariana/adv160420017week4/util/util.kt
@@ -1,8 +1,17 @@
 package com.mariana.adv160420017week4.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.QuickContactBadge
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.mariana.adv160420017week4.R
+import com.mariana.adv160420017week4.view.MainActivity
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.lang.Exception
@@ -23,3 +32,32 @@ fun ImageView.loadImage(url: String?, progressBar: ProgressBar){
             }
         })
 }
+
+fun showNotification(title:String, content:String, icon:Int){
+    val channelId = "${MainActivity.instance?.packageName}-${MainActivity.instance?.getString(R.string.app_name)}"
+
+    val builder = NotificationCompat.Builder(MainActivity.instance!!.applicationContext, channelId).apply {
+        setSmallIcon(icon)
+        setContentTitle(title)
+        setContentText(content)
+        setStyle(NotificationCompat.BigTextStyle())
+        priority = NotificationCompat.PRIORITY_DEFAULT
+        setAutoCancel(true)
+    }
+
+    val manager = NotificationManagerCompat.from(MainActivity.instance!!.applicationContext.applicationContext!!)
+    manager.notify(1001, builder.build())
+}
+
+fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val channelId = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
+        channel.setShowBadge(showBadge)
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
--- app/src/main/java/com/mariana/adv160420017week4/view/MainActivity.kt
+++ app/src/main/java/com/mariana/adv160420017week4/view/MainActivity.kt
@@ -2,11 +2,83 @@ package com.mariana.adv160420017week4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mariana.adv160420017week4.R
+import com.mariana.adv160420017week4.util.createNotificationChannel
+import com.mariana.adv160420017week4.util.showNotification
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    init {
+        instance = this
+    }
+
+    companion object {
+        var instance:MainActivity ?= null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT,
+            false, getString(R.string.app_name), "App Student Data")
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val observable = Observable.timer(5, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{
+                    Log.d("Messages", "five seconds")
+                    showNotification("Dummy", "A new notification appeared", R.drawable.ic_baseline_mail_24)
+                }
+        }
+
+        val observable = Observable.just("a stream of data", "hellow", "world")
+
+        val observer = object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d("Messages", "begin subscribe")
+            }
+
+            override fun onNext(t: String) {
+                Log.d("Messages", "data: $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("Messages", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("Messages", "complete")
+            }
+
+        }
+
+        observable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer)
+
+        //ini coding singkatnya
+//        Observable.just("a stream of data", "hellow", "world").subscribe(
+//            { Log.d("Messages", it) },
+//            { Log.e("Messages", it.message.toString()) },
+//            { Log.d("Messages", "Completed") }
+//        )
+
+//        observable.timer(5, TimeUnit.SECONDS)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(observer)
     }
 }
